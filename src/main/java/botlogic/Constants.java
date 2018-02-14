853aa065947985c539240359d0729e15bddfeb72
Alex Lut <pevcheck@yandex.ru>
2018-02-14 07:40:37 +0300

    [UPDATE] - Тест с токеном.

--- src/main/java/botlogic/Constants.java
+++ src/main/java/botlogic/Constants.java
@@ -2,7 +2,7 @@ package botlogic;
 
 public class Constants {
 
-    public static final String BOT_TOKEN = "";
+    public static final String BOT_TOKEN = "540440766:AAFT0rjxt8gJ2p9RZ9wxpP5W-R_elK0gyUE";
     public static final String BASE_URL = "https://api.telegram.org/file/bot";
     public static final String DOWNLOAD_DIRECTORY = "C:\\Users\\admin\\Desktop\\Telegram_downloaded_files\\";
 }
